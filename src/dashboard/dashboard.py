3c9a9f2b400143a4a2bf335aded74848d9e00833
dgscwhite <cwhite1716@gmail.com>
2024-02-07 19:49:53 +0000

    Changes made after review

--- src/dashboard/dashboard.py
+++ src/dashboard/dashboard.py
@@ -1,7 +1,9 @@
-#Stats Dashboard
+"""dashboard.py - Streamlit app for displaying the stats dashboard.
+"""
 
-import streamlit as st
+from log_keeper.get_config import get_config
 import pymongo
+import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -10,44 +12,49 @@ st.sidebar.markdown("# 661 VGS Dashboard")
 
 # Function to fetch data from MongoDB
 def fetch_data_from_mongodb():
+    """Fetch data from MongoDB and return as a DataFrame.
+    Returns:
+        pd.DataFrame: The data from MongoDB."""
 
     # Construct the MongoDB connection URI
-    DB_USERNAME = "661vgs"
-    DB_PASSWORD = "Viking2022"
-    DB_HOSTNAME = "661vgs.pcc7txt.mongodb.net"
-    db_url = f"mongodb+srv://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOSTNAME}/?retryWrites=true&w=majority"
+    db_config = get_config()
+    DB_HOSTNAME = db_config["DB_HOSTNAME"]
+    DB_USERNAME = db_config["DB_USERNAME"]
+    DB_PASSWORD = db_config["DB_PASSWORD"]
+    DB_COLLECTION_NAME = db_config["DB_COLLECTION_NAME"]
+    DB_NAME = db_config["DB_NAME"]
 
     # Connect to MongoDB
     client = pymongo.MongoClient(db_url)
-    db = client["661vgs"]
-    collection = db["log_sheets"]
-
-    # Retrieve data from the collection
-    cursor = collection.find({})
-
-    # Convert cursor to list of dictionaries
-    data_list = list(cursor)
+    db = client[DB_NAME]
+    collection = db[DB_COLLECTION_NAME]
 
     # Convert list of dictionaries to DataFrame
-    df = pd.DataFrame(data_list)
+    df = pd.DataFrame(collection.find())
 
     return df
 
-# Main Streamlit App Code
+
 def main():
+    """Main Streamlit App Code."""
 
     # Fetch data from MongoDB
     df = fetch_data_from_mongodb()
 
     # Filter by AircraftCommander
-    commander = st.sidebar.selectbox("Filter by AircraftCommander", df["AircraftCommander"].unique())
+    commander = st.sidebar.selectbox(
+        "Filter by AircraftCommander", 
+        df["AircraftCommander"].unique()
+    )
     filtered_df = df[df["AircraftCommander"] == commander]
 
     # Group by AircraftCommander and count launches
     launches_by_commander = df.groupby("AircraftCommander").size()
 
     # Sort launches by commander in descending order
-    launches_by_commander_sorted = launches_by_commander.sort_values(ascending=False)
+    launches_by_commander_sorted = launches_by_commander.sort_values(
+        ascending=False
+    )
 
     # Create a custom bar chart with text color
     fig, ax = plt.subplots()
@@ -55,8 +62,9 @@ def main():
     ax.set_ylabel('Number of Launches')
     ax.set_xlabel('Aircraft Commander')
     ax.set_title('Launches by Aircraft Commander')
-    ax.tick_params(axis='x', rotation=90, labelcolor='black')  # Change label color
-    ax.tick_params(axis='y', labelcolor='black')  # Change label color
+    # Change label color
+    ax.tick_params(axis='x', rotation=90, labelcolor='black')
+    ax.tick_params(axis='y', labelcolor='black')
     st.pyplot(fig)
 
     # Display data in Streamlit
@@ -65,7 +73,12 @@ def main():
     # Add a button to manually refresh data
     if st.button('Refresh Data'):
         data = fetch_data_from_mongodb()
-        st.write(data)
+        st.dataframe(
+            data=data,
+            hide_index=True,
+    )
+
 
 if __name__ == '__main__':
     main()
+    
--- src/dashboard/pages/gif_tracker.py
+++
@@ -1,8 +0,0 @@
-#my website
-
-import streamlit as st
-import pandas as pd
-import numpy as np
-
-st.markdown("# GIF Tracker")
-st.sidebar.markdown("# GIF Tracker")
