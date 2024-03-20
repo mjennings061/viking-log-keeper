7cf816186a3641475300b762cffc99128c0de55f
Michael Jennings <michael.jennings@b-secur.com>
2024-03-20 00:13:01 +0000

    Added quarterly summaries

--- src/dashboard/dashboard.py
+++ src/dashboard/dashboard.py
@@ -40,7 +40,7 @@ def format_data_for_table(raw_df: pd.DataFrame) -> pd.DataFrame:
     data_df = data_df.sort_values(by="Date", ascending=False)
 
     # Format the date.
-    data_df["Date"] = data_df["Date"].dt.strftime("%Y-%m-%d")
+    data_df["Date"] = data_df["Date"].dt.strftime("%d %b %y")
 
     # Convert the FlightTime (minutes) to a string in HH:MM format.
     data_df["FlightTime"] = data_df["FlightTime"].apply(
@@ -120,7 +120,7 @@ def plot_launches_by_commander(df: pd.DataFrame):
     st.altair_chart(chart, use_container_width=True)
 
 
-def show_launches_by_commander(df: pd.DataFrame, commander: str):
+def show_logbook_helper(df: pd.DataFrame, commander: str):
     """Show the number of launches by AircraftCommander in a table.
 
     Args:
@@ -129,7 +129,21 @@ def show_launches_by_commander(df: pd.DataFrame, commander: str):
     """
     # Filter the data by AircraftCommander, if specified.
     if commander:
+        # Get launches where the pilot is commander.
         filtered_df = df[df["AircraftCommander"] == commander]
+
+        # Get launches where the pilot is second pilot and the
+        # duty contains SCT or AGT.
+        second_pilot_df = df[df["SecondPilot"] == commander]
+        sct_df = second_pilot_df[second_pilot_df["Duty"].str.contains(
+            "SCT|AGT", case=False
+        )]
+
+        # Merge the commander and sct dataframes.
+        filtered_df = pd.concat([filtered_df, sct_df])
+
+        # Sort the data by date in descending order.
+        filtered_df = filtered_df.sort_values(by="Date", ascending=False)
     else:
         filtered_df = df
         commander = "All"
@@ -193,6 +207,104 @@ def date_filter(df: pd.DataFrame) -> pd.DataFrame:
     return filtered_df
 
 
+def quarterly_summary(df: pd.DataFrame,
+                      commander: str,
+                      quarter: str) -> pd.DataFrame:
+    """ Show a quarterly summary of the number of launches
+    for each AircraftCommander.
+
+    Args:
+        df (pd.DataFrame): The data to be summarized.
+        commander (str): The AircraftCommander to filter by.
+        quarter (str): The quarter to display."""
+
+    # Get all elements where the pilot is commander.
+    commander_df = df[df["AircraftCommander"] == commander]
+
+    # Get elements where the duty contains SCT or AGT and the pilot
+    # is second pilot.
+    sct_df = df[df["Duty"].str.contains(
+        "SCT|AGT", case=False
+    )]
+    sct_df = sct_df[sct_df["SecondPilot"] == commander]
+
+    # Merge the commander and sct dataframes.
+    commander_df = pd.concat([commander_df, sct_df])
+
+    # Extract the quarter from the date.
+    quarterly_df = commander_df.copy()
+    quarterly_df["Quarter"] = quarterly_df["Date"].dt.to_period("Q")
+    quarterly_df = quarterly_df[quarterly_df["Quarter"] == quarter]
+    quarterly_df = quarterly_df.drop(columns=["Quarter"])
+
+    # Find the last date where PLF was true. This is the last date where:
+    # - 'SecondPilot' is commander
+    # - 'PLF' is true
+    # - 'Duty' contains 'SCT'
+    # Find the last SCT and PLF dates.
+    sct_in_quarter = sct_df[
+        sct_df["Date"].dt.to_period("Q") <= quarter
+    ]
+
+    if sct_in_quarter.empty:
+        last_sct = "N/A"
+        last_plf = "N/A"
+    else:
+        last_sct = sct_in_quarter["Date"].max().strftime("%d %b %y")
+        last_plf = sct_in_quarter["Date"].max().strftime("%d %b %y")
+
+    # Create a summary table for the selected quarter.
+    # Count launches and hours flown by AircraftCommander.
+    summary = pd.DataFrame({
+        "Aircraft Commander": commander,
+        "Launches": quarterly_df.shape[0],
+        "Hours": quarterly_df["FlightTime"].sum(),
+        "Last SCT": last_sct,
+        "Last PLF": last_plf
+    }, index=[0])
+
+    # Convert the FlightTime (minutes) to a string in HH:MM format.
+    summary["Hours"] = summary["Hours"].apply(
+        lambda x: f"{x//60}:{x%60:02d}"
+    )
+
+    # Display the summary table.
+    st.header("Quarterly Summary Helper")
+    st.dataframe(
+        data=summary,
+        hide_index=True,
+    )
+
+
+def plot_all_launches(df: pd.DataFrame):
+    """ Plot all launches in the data.
+
+    Args:
+        df (pd.DataFrame): The data to be plotted.
+    """
+    # Sort the data by date in descending order.
+    df = df.sort_values(by="Date", ascending=False)
+    df = df.drop(columns=["_id"])
+
+    # Format the date.
+    df["Date"] = df["Date"].dt.strftime("%d %b %y")
+
+    # Convert the FlightTime (minutes) to a string in HH:MM format.
+    df["FlightTime"] = df["FlightTime"].apply(
+        lambda x: f"{x//60}:{x%60:02d}"
+    )
+
+    # Format TakeOffTime and LandingTime.
+    df["TakeOffTime"] = df["TakeOffTime"].dt.strftime("%H:%M")
+    df["LandingTime"] = df["LandingTime"].dt.strftime("%H:%M")
+
+    # Make date the first column.
+    df = df[["Date"] + [col for col in df.columns if col != "Date"]]
+
+    # Plot all data.
+    st.dataframe(df, use_container_width=True)
+
+
 def main():
     """Main Streamlit App Code."""
     # Set the page title.
@@ -223,11 +335,37 @@ def main():
     # Add a date filter to the sidebar.
     filtered_df = date_filter(df)
 
-    # Plot the number of launches by unique AircraftCommander.
-    plot_launches_by_commander(filtered_df)
+    tabs = ["Statistics", "All Data"]
+    stats_tab, all_data_tab = st.tabs(tabs)
 
-    # Logbook helper by AircraftCommander.
-    show_launches_by_commander(filtered_df, commander)
+    with stats_tab:
+
+        # Plot the number of launches by unique AircraftCommander.
+        plot_launches_by_commander(filtered_df)
+
+        # Logbook helper by AircraftCommander.
+        show_logbook_helper(filtered_df, commander)
+
+        # Show a quarterly summary of the number of launches
+        # for each AircraftCommander.
+        st.sidebar.markdown("## Quarterly Summary")
+
+        # Create a list of quarters from the data.
+        quarters = filtered_df["Date"].dt.to_period("Q").unique()
+
+        quarter = st.sidebar.selectbox(
+            "Select Quarter",
+            quarters,
+            index=None,
+            help="Select the quarter to display."
+        )
+
+        # Filter the data by the selected quarter.
+        if quarter and commander:
+            quarterly_summary(filtered_df, commander, quarter)
+
+    with all_data_tab:
+        plot_all_launches(filtered_df)
 
 
 if __name__ == '__main__':
