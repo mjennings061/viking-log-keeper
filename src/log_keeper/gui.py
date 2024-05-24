503a7bb02900454dea7704b748b0f334e9c4aa44
mjennings061 <mjennings061@gmail.com>
2024-05-24 18:33:36 +0100

    Minimum GUI

---
+++ src/log_keeper/gui.py
@@ -0,0 +1,34 @@
+"""gui.py - Flet-based GUI for the log_keeper application."""
+
+import flet as ft
+
+
+def main(page: ft.Page):
+    page.title = "Flet counter example"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+
+    txt_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
+
+    def minus_click(e):
+        txt_number.value = str(int(txt_number.value) - 1)
+        page.update()
+
+    def plus_click(e):
+        txt_number.value = str(int(txt_number.value) + 1)
+        page.update()
+
+    page.add(
+        ft.Row(
+            [
+                ft.IconButton(ft.icons.REMOVE, on_click=minus_click),
+                txt_number,
+                ft.IconButton(ft.icons.ADD, on_click=plus_click),
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+        )
+    )
+
+
+if __name__ == "__main__":
+    # Run the Flet app.
+    ft.app(main)
