c7c16be68845d06c042302a38b49864933356f6b
Michael Jennings <michael.jennings@b-secur.com>
2024-07-17 00:12:40 +0100

    Move `run_log_keeper.bat`

--- setup.py
+++ setup.py
@@ -40,7 +40,8 @@ class PostInstallCommand(install):
         BAT_FILE_NAME = 'run_log_keeper.bat'
 
         # Define the source and destination paths
-        source = Path(__file__).parent / 'scripts' / BAT_FILE_NAME
+        source = Path(__file__).parent / 'log_keeper' / 'scripts' / \
+            BAT_FILE_NAME
 
         # Find the desktop. If the user has OneDrive, use that instead.
         desktop = Path.home() / 'Desktop'
