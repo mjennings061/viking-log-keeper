ee07d3ec7277a4f8951957895318c6bf34d86eff
Michael Jennings <mjennings061@gmail.com>
2024-08-14 20:10:23 +0100

    Ensuring secret is taken from environment if not from secrets.toml

--- src/dashboard/main.py
+++ src/dashboard/main.py
@@ -252,11 +252,19 @@ def show_data_dashboard(db: Database):
 
 def login(username: str, password: str):
     """Login to the dashboard."""
+    # Get URI from secrets.
+    if st.secrets["URI"] is not None:
+        # Get the URI from Streamlit secrets.
+        uri = st.secrets["URI"]
+    else:
+        # Get the URI from the environment.
+        uri = os.getenv("URI")
+
     # Create the DB user.
     db_user = DbUser(
         username=username,
         password=password,
-        uri=st.secrets["URI"],
+        uri=uri,
     )
 
     # Validate the password.
