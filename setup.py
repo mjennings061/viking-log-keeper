fe5937443cf82d717a2a407ee5d32aeb040d4b49
dgscwhite <138334420+dgscwhite@users.noreply.github.com>
2024-10-18 22:17:00 +0100

    Update readme

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ class PostInstallCommand(install):
 def run_setup():
     setup(
         name="viking-log-keeper",
-        version="2.3.0",
+        version="2.2.6",
         packages=find_packages(where="src"),
         package_dir={"": "src"},
         url="https://github.com/mjennings061/viking-log-keeper",
